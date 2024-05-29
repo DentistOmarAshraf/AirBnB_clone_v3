7c266db47f44e50645dbb438b545d29578bfd142
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-29 22:28:56 +0000

    Review API

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -12,7 +12,8 @@ from json import dumps
 from flask import abort, make_response, request
 
 
-@app_views.route("/places/<place_id>/reviews", strict_slashes=False, methods=['GET', 'POST'])
+@app_views.route("/places/<place_id>/reviews", strict_slashes=False,
+                 methods=['GET', 'POST'])
 def place_rev(place_id=None):
     if request.method == "GET":
         data = []
@@ -70,5 +71,56 @@ def place_rev(place_id=None):
         return res
 
 
-#@app_views.route("/reviews/<review_id>", strict_slashes=False, methods=['GET'])
-#def review_about(review_id=None):
+@app_views.route("/reviews/<review_id>", strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
+def review_about(review_id=None):
+    if review_id:
+        if request.method == "GET":
+            data = []
+            for rev in storage.all(Review).values():
+                if rev.id == review_id:
+                    data.append(rev.to_dict())
+
+            if len(data) == 0:
+                abort(404)
+
+            res = make_response(dumps(data[0], indent=4), 200)
+            res.headers['Content-type'] = 'application/json'
+            return res
+
+        if request.method == "DELETE":
+            dlt = False
+            for rev in storage.all(Review).values():
+                if rev.id == review_id:
+                    storage.delete(rev)
+                    storage.save()
+                    dlt = True
+            if dlt is False:
+                abort(404)
+            res = make_response(dumps({}), 200)
+            res.headers['Content-type'] = 'application/json'
+            return res
+
+        if request.method == "PUT":
+            try:
+                data = request.get_json()
+            except Exception:
+                return make_response("Not a JSON", 400)
+
+            change = False
+            for rev in storage.all(Review).values():
+                if rev.id == review_id:
+                    for k, v in data.items():
+                        x = ['id', 'user_id', 'created_at', 'updated_at']
+                        if k not in x:
+                            setattr(rev, k, v)
+                    change = True
+                    rev.save()
+                    to_ret = rev.to_dict()
+
+                if change is False:
+                    abort(404)
+
+                res = make_response(dumps(to_ret, indent=4), 200)
+                res.headers['Content-type'] = 'application/json'
+                return res
