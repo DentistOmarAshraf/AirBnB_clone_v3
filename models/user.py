452432fa402199c23e5ef9594ca3357bc4d1e33c
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-31 20:04:16 +0000

    Try Hashing Idea

--- models/user.py
+++ models/user.py
@@ -8,21 +8,30 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
     """Representation of a user """
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, passwd):
+        self._password = hashlib.md5(passwd.encode()).hexdigest()
+
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column('password',String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
