8b2ab430b83327dc636083b5b31b39ab8d665340
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-31 04:34:21 +0000

    [Edit] Amenity storage.get

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -37,27 +37,20 @@ def amenity_about(amenity_id=None):
 
     else:
         if request.method == "GET":
-            data = []
-            for amen in storage.all(Amenity).values():
-                if amen.id == amenity_id:
-                    data.append(amen.to_dict())
-            if len(data) == 0:
+            data = storage.get(Amenity, amenity_id)
+            if data is None:
                 abort(404)
-
-            res = make_response(dumps(data[0], indent=3), 200)
+            data = data.to_dict()
+            res = make_response(dumps(data, indent=3), 200)
             res.headers['Content-type'] = 'application/json'
             return res
 
         if request.method == "DELETE":
-            dlt = False
-            for amen in storage.all(Amenity).values():
-                if amen.id == amenity_id:
-                    storage.delete(amen)
-                    storage.save()
-                    dlt = True
-
-            if dlt is False:
+            amen = storage.get(Amenity, amenity_id)
+            if amen is None:
                 abort(404)
+            storage.delete(amen)
+            storage.save()
 
             res = make_response(dumps({}), 200)
             res.headers['Content-type'] = 'application/json'
@@ -69,19 +62,17 @@ def amenity_about(amenity_id=None):
             except Exception:
                 return make_response("Not a JSON", 400)
 
-            change = False
-            for amen in storage.all(Amenity).values():
-                if amen.id == amenity_id:
-                    for k, v in data.items():
-                        x = ['id', 'created_at', 'updated_at']
-                        if k not in x:
-                            setattr(amen, k, v)
-                    storage.save()
-                    to_ret = amen.to_dict()
-                    change = True
-
-            if change is False:
+            amen = storage.get(Amenity, amenity_id)
+            if amen is None:
                 abort(404)
+
+            for k, v in data.items():
+                x = ['id', 'created_at', 'updated_at']
+                if k not in x:
+                    setattr(amen, k, v)
+            storage.save()
+            to_ret = amen.to_dict()
+
             res = make_response(dumps(to_ret, indent=4), 200)
             res.headers['Content-type'] = 'application/json'
             return res
