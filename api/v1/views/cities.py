ad307029a68797c588f97ca1f6f65fdf140aca17
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-31 04:17:14 +0000

    [Edit] City api storage.get

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -23,26 +23,20 @@ def about_cities(city_id=None):
             return res
     else:
         if request.method == 'GET':
-            found = False
-            for city in storage.all(City).values():
-                if city_id == city.id:
-                    data = city.to_dict()
-                    found = True
-            if found is False:
+            data = storage.get(City, city_id)
+            if data is None:
                 abort(404)
+            data = data.to_dict()
             res = make_response(dumps(data, indent=4), 200)
             res.headers['Content-type'] = 'application/json'
             return res
 
         if request.method == 'DELETE':
-            deleted = False
-            for city in storage.all(City).values():
-                if city_id == city.id:
-                    storage.delete(city)
-                    storage.save()
-                    deleted = True
-            if deleted is False:
+            city = storage.get(City, city_id)
+            if city is None:
                 abort(404)
+            storage.delete(city)
+            storage.save()
             res = make_response(dumps({}), 200)
             res.headers['Content-type'] = 'application/json'
             return res
@@ -53,19 +47,22 @@ def about_cities(city_id=None):
             except Exception:
                 return make_response("Not a JSON", 400)
 
-            change = False
-            for city in storage.all(City).values():
-                if city_id == city.id:
-                    for k, v in data.items():
-                        x = ['id', 'created_at', 'updated_at']
-                        if k not in x:
-                            setattr(city, k, v)
-                    change = True
-                    to_ret = city.to_dict()
-                    storage.save()
-
-            if change is False:
+            city = storage.get(City, city_id)
+            if city is None:
                 abort(404)
+
+            change = False
+            for k, v in data.items():
+                x = ['id', 'created_at', 'updated_at']
+                if k not in x:
+                    setattr(city, k, v)
+                    change = True
+            if change is False:
+                abort(400)
+
+            storage.save()
+            to_ret = city.to_dict()
+
             res = make_response(dumps(to_ret, indent=3), 200)
             res.headers['Content-type'] = 'application/json'
             return res
@@ -75,13 +72,10 @@ def about_cities(city_id=None):
                  methods=['GET', 'POST'])
 def cities_by_state(state_id=None):
     if request.method == 'GET':
-        all_state = []
-        for st in storage.all(State).values():
-            if state_id == st.id:
-                all_state.append(st)
-        if len(all_state) == 0:
+        all_state = storage.get(State, state_id)
+        if all_state is None:
             abort(404)
-        data = [city.to_dict() for city in all_state[0].cities]
+        data = [city.to_dict() for city in all_state.cities]
         res = make_response(dumps(data, indent=4), 200)
         res.headers['Content-type'] = 'application/json'
         return res
@@ -95,14 +89,11 @@ def cities_by_state(state_id=None):
         if 'name' not in data:
             return make_response("Missing name", 400)
 
-        the_st = []
-        for st in storage.all(State).values():
-            if st.id == state_id:
-                the_st.append(st)
-        if len(the_st) == 0:
+        the_st = storage.get(State, state_id)
+        if the_st is None:
             abort(404)
         new_city = City(**data)
-        the_st[0].cities.append(new_city)
+        the_st.cities.append(new_city)
         storage.save()
         to_ret = new_city.to_dict()
         if 'state' in to_ret:
