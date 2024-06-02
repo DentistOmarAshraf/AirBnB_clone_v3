63a7c2fc6ffcf958a90b41c8a310f8b66a5fdadc
DentistOmarAshraf <omar_ashraf@live.com>
2024-06-02 04:27:38 +0000

    [Edit] modify API for maintaince

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,73 +6,78 @@ from .__init__ import app_views
 from models import storage
 from models.state import State
 from flask import make_response, request, abort
-import json
+from json import dumps
 
 
-@app_views.route("/states", strict_slashes=False, methods=['GET', 'POST'])
+@app_views.route("/states", strict_slashes=False, methods=['GET'])
+def get_states():
+    data = [state.to_dict() for state in storage.all(State).values()]
+    res = make_response(dumps(data, indent=4), 200)
+    res.headers['Content-type'] = 'application/json'
+    return res
+
+
+@app_views.route("/states", strict_slashes=False, methods=['POST'])
+def post_states():
+    try:
+        data = request.get_json()
+    except Exception:
+        return make_response("Not a JSON", 400)
+
+    if 'name' not in data:
+        return make_response("Missing name", 400)
+    new_state = State(**data)
+    new_state.save()
+    res = make_response(dumps(new_state.to_dict(), indent=4), 201)
+    res.headers['Content-type'] = 'application/json'
+    return res
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=['GET'])
+def get_states_id(state_id):
+    data = storage.get(State, state_id)
+    if data is None:
+        abort(404)
+    res = make_response(dumps(data.to_dict(), indent=3), 200)
+    res.headers['Content-type'] = 'application/json'
+    return res
+
+
 @app_views.route("/states/<state_id>", strict_slashes=False,
-                 methods=['GET', 'DELETE', 'PUT'])
-def about_states(state_id=None):
-    if state_id is None:
-        if request.method == 'GET':
-            data = [state.to_dict() for state in storage.all(State).values()]
-            res = make_response(json.dumps(data, indent=4), 200)
-            res.headers['Content-type'] = 'application/json'
-            return res
+                 methods=['DELETE'])
+def delete_state_id(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    for city in state.cities:
+        storage.delete(city)
+    storage.delete(state)
+    storage.save()
+    res = make_response(dumps({}), 200)
+    res.headers['Content-type'] = 'application/json'
+    return res
 
-        if request.method == 'POST':
-            try:
-                data = request.get_json()
-            except Exception:
-                return make_response("Not a JSON", 400)
-            if 'name' not in data:
-                return make_response("Missing name", 400)
-            new_state = State(**data)
-            new_state.save()
-            res = make_response(json.dumps(new_state.to_dict(), indent=4), 201)
-            res.headers['Content-type'] = 'application/json'
-            return res
 
-    else:
-        if request.method == 'GET':
-            data = storage.get(State, state_id)
-            if data is None:
-                abort(404)
-            res = make_response(json.dumps(data.to_dict(), indent=3), 200)
-            res.headers['Content-type'] = 'application/json'
-            return res
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=['PUT'])
+def put_state_id(state_id):
+    try:
+        data = request.get_json()
+    except Exception:
+        return make_response("Not a JSON", 400)
 
-        if request.method == 'DELETE':
-            state = storage.get(State, state_id)
-            if state is None:
-                abort(404)
-            for city in state.cities:
-                storage.delete(city)
-            storage.delete(state)
-            storage.save()
-            res = make_response(json.dumps({}), 200)
-            res.headers['Content-type'] = 'application/json'
-            return res
-
-        if request.method == 'PUT':
-            try:
-                data = request.get_json()
-            except Exception:
-                return make_response("Not a JSON", 400)
-
-            state = storage.get(State, state_id)
-            if state is None:
-                abort(404)
-            changed = False
-            for k, v in data.items():
-                x = ['id', 'created_at', 'updated_at']
-                if k not in x:
-                    setattr(state, k, v)
-                    changed = True
-            if changed is False:
-                abort(400)
-            to_ret = state.to_dict()
-            storage.save()
-            res = make_response(json.dumps(to_ret, indent=3), 200)
-            res.headers['Content-type'] = 'application/json'
-            return res
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    changed = False
+    for k, v in data.items():
+        x = ['id', 'created_at', 'updated_at']
+        if k not in x:
+            setattr(state, k, v)
+            changed = True
+    if changed is False:
+        abort(400)
+    to_ret = state.to_dict()
+    storage.save()
+    res = make_response(dumps(to_ret, indent=3), 200)
+    res.headers['Content-type'] = 'application/json'
+    return res
